a83dc6886a03d4d73ca5e5769b1cfb7e919439cf
zubaria <zubariaayub634@gmail.com>
2020-10-23 09:47:54 +0500

    Shows popup on tapping on camera

--- lib/screens/collisionPreventionScreen/collisionPreventionScreen.dart
+++ lib/screens/collisionPreventionScreen/collisionPreventionScreen.dart
@@ -1,28 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:uvea/components/camera.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:uvea/components/uveaTextButton.dart';
 
 class CollisionPreventionScreen extends StatefulWidget {
   final List<CameraDescription> cameras;
 
-  CollisionPreventionScreen({Key key,this.cameras}) : super(key: key);
+  CollisionPreventionScreen({Key key, this.cameras}) : super(key: key);
   @override
-  _CollisionPreventionScreenState createState() => _CollisionPreventionScreenState();
+  _CollisionPreventionScreenState createState() =>
+      _CollisionPreventionScreenState();
 }
 
 class _CollisionPreventionScreenState extends State<CollisionPreventionScreen> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.orange,
       appBar: AppBar(
-        title: Text("Main Perspective"),
+        title: Text("Collision Prevention"),
         centerTitle: true,
         backgroundColor: Colors.transparent,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
+        child: MaterialButton(
+          onPressed: () {
+            print("tapped on camera");
+            Alert(
+                style: AlertStyle(
+                  backgroundColor: Colors.blueGrey,
+                ),
+                context: context,
+                title:
+                    "You tapped on the camera. This is a dummy. You will see popup collision prevention messages in the next version of the product.",
+                content: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 10,
+                    ),
+                    UveaTextButton(
+                      "OK",
+                      onPressed: () async {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+                buttons: [
+                  DialogButton(
+                    color: Colors.white,
+                    height: 0,
+                  ),
+                ]).show();
+          },
           child: Camera(
             widget.cameras,
           ),
