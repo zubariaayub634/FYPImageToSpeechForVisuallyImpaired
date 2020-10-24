2de43bcf7a4428b1aae46f071000dfdba94a5287
i170326 <i170326@nu.edu.pk>
2020-10-24 06:54:19 +0500

    ?

--- lib/components/bodyText.dart
+++ lib/components/bodyText.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class BodyText extends Text {
-  BodyText(
-    String text,
-  ) : super(
-          text,style: TextStyle(color: Colors.red,),textAlign: TextAlign.center,
+  BodyText(String text, {double size = 12})
+      : super(
+          text, style: TextStyle(color: Colors.red, fontSize: size),
+          textAlign: TextAlign.center,
           //TODO: add styling for body text here
         );
 
