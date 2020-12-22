a929653b52a3373b3d119f00612191c5b88e2f6f
zubaria <zubariaayub634@gmail.com>
2020-12-22 17:08:36 +0500

    refactors code

--- lib/screens/moneyClassifierScreen/moneyClassifierScreen.dart
+++ lib/screens/moneyClassifierScreen/moneyClassifierScreen.dart
@@ -5,8 +5,6 @@ import 'package:flutter/services.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:tflite/tflite.dart';
 import 'package:uvea/components/uveaTextButton.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart';
 import 'package:uvea/components/camera.dart';
 import 'package:uvea/components/bodyText.dart';
 import 'moneyClassifierModel.dart';
@@ -40,66 +38,6 @@ class MoneyClassifierScreen extends StatelessWidget {
             await camera.controller.startImageStream((CameraImage img) async {
               camera.controller.stopImageStream();
               print("camera stream going on");
-              /*if (!isDetecting) {
-                isDetecting = true;
-
-                int startTime = new DateTime.now().millisecondsSinceEpoch;
-
-                if (widget.model == mobilenet) {
-                  Tflite.runModelOnFrame(
-                    bytesList: img.planes.map((plane) {
-                      return plane.bytes;
-                    }).toList(),
-                    imageHeight: img.height,
-                    imageWidth: img.width,
-                    numResults: 2,
-                  ).then((recognitions) {
-                    int endTime = new DateTime.now().millisecondsSinceEpoch;
-                    print("Detection took ${endTime - startTime}");
-
-                    widget.setRecognitions(recognitions, img.height, img.width);
-
-                    isDetecting = false;
-                  });
-                } else if (widget.model == posenet) {
-                  Tflite.runPoseNetOnFrame(
-                    bytesList: img.planes.map((plane) {
-                      return plane.bytes;
-                    }).toList(),
-                    imageHeight: img.height,
-                    imageWidth: img.width,
-                    numResults: 2,
-                  ).then((recognitions) {
-                    int endTime = new DateTime.now().millisecondsSinceEpoch;
-                    print("Detection took ${endTime - startTime}");
-
-                    widget.setRecognitions(recognitions, img.height, img.width);
-
-                    isDetecting = false;
-                  });
-                } else {
-                  Tflite.detectObjectOnFrame(
-                    bytesList: img.planes.map((plane) {
-                      return plane.bytes;
-                    }).toList(),
-                    model: widget.model == yolo ? "YOLO" : "SSDMobileNet",
-                    imageHeight: img.height,
-                    imageWidth: img.width,
-                    imageMean: widget.model == yolo ? 0 : 127.5,
-                    imageStd: widget.model == yolo ? 255.0 : 127.5,
-                    numResultsPerClass: 1,
-                    threshold: widget.model == yolo ? 0.2 : 0.4,
-                  ).then((recognitions) {
-                    int endTime = new DateTime.now().millisecondsSinceEpoch;
-                    print("Detection took ${endTime - startTime}");
-
-                    widget.setRecognitions(recognitions, img.height, img.width);
-
-                    isDetecting = false;
-                  });
-                }
-              }*/
-              //print("image stream started");
               try {
                 int startTime = new DateTime.now().millisecondsSinceEpoch;
                 await Tflite.runModelOnFrame(
@@ -127,69 +65,43 @@ class MoneyClassifierScreen extends StatelessWidget {
                       title: "",
                       content: moneyModel.modelLoaded
                           ? Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            recognitions.toString(),
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          UveaTextButton(
-                            "OK",
-                            onPressed: () async {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                        ],
-                      )
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  recognitions.toString(),
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                UveaTextButton(
+                                  "OK",
+                                  onPressed: () async {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                              ],
+                            )
                           : Text(
-                        "Model is loading, please try again.",
-                        style: TextStyle(color: Colors.white),
-                      ),
+                              "Model is loading, please try again.",
+                              style: TextStyle(color: Colors.white),
+                            ),
                       buttons: [
                         DialogButton(
                           color: Colors.white,
                           height: 0,
                         ),
                       ]).show();
-
-                  //widget.setRecognitions(recognitions, img.height, img.width);
-
-                  //isDetecting = false;
                 });
-
               } on PlatformException catch (e) {
                 print(e.message);
               }
             });
-
-            /*try {
-              // Ensure that the camera is initialized.
-              //await _initializeControllerFuture;
-
-              // Construct the path where the image should be saved using the path
-              // package.
-              final path = join(
-                // Store the picture in the temp directory.
-                // Find the temp directory using the `path_provider` plugin.
-                  (await getTemporaryDirectory()).path,
-            '${DateTime.now()}.png',
-            );
-
-            // Attempt to take a picture and log where it's been saved.
-            await camera.controller.takePicture(path);
-            print("image saved at $path");
-            } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-            }*/
           },
           child: camera,
         ),
